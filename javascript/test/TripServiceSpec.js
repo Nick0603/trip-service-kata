dab464dabf2b63ee0b348d2dc0b670a7344cfabd
陳建宇 <chenjianyu@chenjianyude-MacBook-Pro.local>
2020-03-07 23:48:16 +0800

    refactor testcase

--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -3,43 +3,49 @@
 let assert = require('assert');
 let TripService = require('../src/TripService');
 let User = require('../src/User');
+let Trip = require('../src/Trip');
+
+const USER_TRIP_DATA = [new Trip(), new Trip()];
+const NOT_LOGIN_USER = null;
+const LOGINNED_USER = new User('Jenny');
+const NO_FRIEND_USER = new User('Nick');
+const WITH_FRIEND_USER = new User('Nick');
+WITH_FRIEND_USER.addFriend(LOGINNED_USER);
 
 describe('TripService', () => {
 
     it('shouldThrowExceptionWhenUserIsNotLoggedIn', () => {
-        const service = new TripServiceUnderTest();
-        assert.throws(() => service.getTripsByUser(), Error, 'User not logged in.');
+        // Setup
+        const service = new TripService();
+        service.getLoggedUser = () => NOT_LOGIN_USER;
+
+        // Exercise
+        // Verify
+        assert.throws(() => service.getTripsByUser(NO_FRIEND_USER), Error, 'User not logged in.');
     });
 
     it('shouldNotReturnTripsWhenLoggedUserIsNotAFriend' , () => {
+        // Setup
         const expected = [];
-        const noAnyFriendUser = new User();
-        const service = new TripServiceUnderTest({loggedUser: 'userId'});
-        const actual = service.getTripsByUser(noAnyFriendUser);
+        const service = new TripService();
+        service.getLoggedUser = () => LOGINNED_USER;
+        service.getTrips = () => USER_TRIP_DATA;
+        assert.notDeepEqual(expected, USER_TRIP_DATA);
+        // Exercise
+        const actual = service.getTripsByUser(NO_FRIEND_USER);
+        // Verify
         assert.deepEqual(actual, expected);
     })
 
     it('shouldReturnTripsWhenLoggedUserIsAFriend', () => {
-        const expected = ['trip1', 'trip2', 'trip3'];
-        const withFriendUser = new User('dummyName', [], ['userId']);
-        const service = new TripServiceUnderTest({loggedUser: 'userId', trips: expected});
-        const actual = service.getTripsByUser(withFriendUser);
+        // Setup
+        const expected = USER_TRIP_DATA;
+        const service = new TripService();
+        service.getLoggedUser = () => LOGINNED_USER;
+        service.getTrips = () => USER_TRIP_DATA;
+        // Exercise
+        const actual = service.getTripsByUser(WITH_FRIEND_USER);
+        // Verify
         assert.deepEqual(actual, expected);
     })
 });
-
-class TripServiceUnderTest extends TripService{
-    constructor({loggedUser, trips} = {}){
-        super();
-        this.loggedUser = loggedUser || null;
-        this.trips = trips || ['trips']
-    }
-
-    getLoggedUser(){
-        return this.loggedUser;
-    }
-
-    getTrips(){
-        return this.trips;
-    }
-}
