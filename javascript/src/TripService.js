50556a9beeadb8d18df31ad50eb3c1246a811b5a
陳建宇 <chenjianyu@chenjianyude-MacBook-Pro.local>
2020-03-08 00:13:59 +0800

    refactor that decrease number of big brackets

--- javascript/src/TripService.js
+++ javascript/src/TripService.js
@@ -8,22 +8,21 @@ class TripService {
         let tripList = [];
         let loggedUser = this.getLoggedUser();
         let isFriend = false;
-        if (loggedUser != null) {
-            let friends = user.getFriends();
-            for (let i=0; i < friends.length; i++) {
-                let friend = friends[i];
-                if (friend == loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            };
-            if (isFriend) {
-                tripList = this.getTrips(user);
-            }
-            return tripList;
-        } else {
+        if (loggedUser == null) {
             throw new Error('User not logged in.');
         }
+        let friends = user.getFriends();
+        for (let i=0; i < friends.length; i++) {
+            let friend = friends[i];
+            if (friend == loggedUser) {
+                isFriend = true;
+                break;
+            }
+        };
+        if (isFriend) {
+            tripList = this.getTrips(user);
+        }
+        return tripList;
     }
 
     getLoggedUser(){
